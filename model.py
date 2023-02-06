896252ca0d38ec16dd6f1a7b6fc5bc98acc6e884
Gawain Gan <41322285+GawainGan@users.noreply.github.com>
2023-02-06 12:10:45 -0800

    Add files via upload

--- model.py
+++ model.py
@@ -22,60 +22,31 @@ class LmModel:
 
     Methods
     -------
-    get_mse():
-        Obtain the mean squared error.
-    get_r_square():
-        Obtain the coefficient of determination.
-    get_coef():
-        Obtain the coefficients of the linear regression model.
-    get_intercept():
-        Obtain the intercept of the linear regression model.
-    get_residuals():
-        Obtain the residuals of the linear regression model.
-    get_equation():
-        Obtain the equation of the linear regression model.
+    get_summary():
+        Obtain MSE, coefficients, intercept, residuals, equation, train score, and test score.
+        Return a dictionary containing all coefficents.
     """
 
-    def __init__(self, X_train, Y_train, X_test, Y_test):
+    def __init__(self, X_train, X_test, y_train, y_test):
         self.X_train = X_train
-        self.Y_train = Y_train
+        self.y_train = y_train
         self.X_test = X_test
-        self.Y_test = Y_test
-        self.model = LinearRegression().fit(self.X_train, self.Y_train)
+        self.y_test = y_test
+        self.model = LinearRegression().fit(self.X_train, self.y_train)
 
-    def get_mse(self):
-        # Obtain the mean squared error.
-        self.mse = mean_squared_error(self.Y_test, self.model.predict(self.X_test))
-        return self.mse
-
-    def get_r_square(self):
-        # Obtain the coefficient of determination.
-        self.r_square = r2_score(self.Y_train, self.model.predict(self.X_train))
-        return self.r_square
-
-    def get_coef(self):
-        # Obtain the coefficients from the linear regression model.
+    def get_summary(self):
+        self.mse = mean_squared_error(self.y_test, self.model.predict(self.X_test))
         self.coef = self.model.coef_
-        return self.coef
-
-    def get_intercept(self):
-        # Obtain the intercept from the linear regression model.
         self.intercept = self.model.intercept_
-        return self.intercept
-
-    def get_residuals(self):
-        # Obtain the residuals.
-        self.residuals = self.Y_test - self.model.predict(self.X_test)
-        return self.residuals
-
-    def get_equation(self):
-        # Obtain the equation of the linear regression model.
+        self.residuals = self.y_test - self.model.predict(self.X_test)
         self.equation = 'y = ' + str(self.intercept)
         for i in range(len(self.coef)):
             self.equation += ' + ' + str(self.coef[i]) + ' * ' + self.X_train.columns[i]
-        return self.equation
-
-
+        self.train_score = self.model.score(self.X_train, self.y_train)
+        self.test_score = self.model.score(self.X_test, self.y_test)
+        return dict({'MSE': self.mse, 'Coefficients': self.coef, 'Intercept': self.intercept, 'Residuals': self.residuals, 
+                     'Equation': self.equation, 'Train score': self.train_score, 'Test score': self.test_score})
+    
 class RfModel:
     """
     A class to perform random forest regression.
@@ -93,38 +64,22 @@ class RfModel:
 
     Methods
     -------
-    get_mse():
-        Obtain the mean squared error.
-    get_r_square():
-        Obtain the coefficient of determination.
-    get_feature_importances():
-        Obtain the feature importances.
-    get_residuals():
-        Obtain the residuals.
+    get_summary():
+        Obtain MSE, feature importances, residuals, train score, and test score.
+        Return a dictionary containing all coefficents.
     """
-    def __init__(self, X_train, Y_train, X_test, Y_test):
+    def __init__(self, X_train, X_test, y_train, y_test):
         self.X_train = X_train
-        self.Y_train = Y_train
+        self.y_train = y_train
         self.X_test = X_test
-        self.Y_test = Y_test
-        self.model = RandomForestRegressor().fit(self.X_train, self.Y_train)
+        self.y_test = y_test
+        self.model = RandomForestRegressor().fit(self.X_train, self.y_train)
 
-    def get_mse(self):
-        # Obtain the mean squared error.
-        self.mse = mean_squared_error(self.Y_test, self.model.predict(self.X_test))
-        return self.mse
-
-    def get_r_square(self):
-        # Obtain the coefficient of determination.
-        self.r_square = r2_score(self.Y_train, self.model.predict(self.X_train))
-        return self.r_square
-
-    def get_feature_importances(self):
-        # Obtain the feature importances.
+    def get_summary(self):
+        self.mse = mean_squared_error(self.y_test, self.model.predict(self.X_test))
         self.feature_importances = self.model.feature_importances_
-        return self.feature_importances
-
-    def get_residuals(self):
-        # Obtain the residuals.
-        self.residuals = self.Y_test - self.model.predict(self.X_test)
-        return self.residuals
+        self.residuals = self.y_test - self.model.predict(self.X_test)
+        self.train_score = self.model.score(self.X_train, self.y_train)
+        self.test_score = self.model.score(self.X_test, self.y_test)
+        return dict({'MSE': self.mse, 'Feature importances': self.feature_importances, 'Residuals': self.residuals, 
+                     'Train score': self.train_score, 'Test score': self.test_score})
